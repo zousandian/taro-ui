205428782157c6374bd6c5f58a010baba26136c4
Garfield Lee <openpgp@550.moe>
2019-12-04 18:23:52 +0800

    feat(taro-ui-demo): 新增 taro-ui-demo 项目

---
+++ packages/taro-ui-demo/src/pages/advanced/calendar/index.tsx
@@ -0,0 +1,193 @@
+import bind from 'bind-decorator'
+import { AtButton, AtCalendar } from '@taroui/components'
+
+import { View } from '@tarojs/components'
+import Taro, { Component } from '@tarojs/taro'
+
+import DocsHeader from '../../components/doc-header'
+import './index.scss'
+
+export default class Index extends Component {
+  public config: Taro.PageConfig = {
+    navigationBarTitleText: 'Taro日历组件展示'
+  }
+
+  state = {
+    now: Date.now(),
+    minDate: '2018/06/11',
+    maxDate: '2020/12/12',
+    multiCurentDate: {
+      start: Date.now()
+    },
+    mark: [
+      {
+        value: '2018/11/11'
+      }
+    ],
+    validDates: [
+      {
+        value: '2019/04/17'
+      },
+      {
+        value: '2019/04/21'
+      },
+      {
+        value: '2019/05/04'
+      },
+      {
+        value: '2019/05/28'
+      }
+    ]
+  }
+
+  @bind
+  private handleClick (key: string, value: string): void {
+    this.setState({
+      [key]: value
+    })
+  }
+
+  @bind
+  private handleDayClick (...arg): void {
+    console.log('handleDayClick', arg)
+  }
+
+  @bind
+  private handleDayLongClick (...arg): void {
+    console.log('handleDayLongClick', arg)
+  }
+
+  @bind
+  private handleDateChange (arg): void {
+    console.log('handleDateChange', arg)
+  }
+
+  @bind
+  private handleMonthChange (arg): void {
+    console.log('handleMonthChange', arg)
+  }
+
+  public render (): JSX.Element {
+    const { now, minDate, maxDate, mark, multiCurentDate, validDates } = this.state
+    return (
+      <View className='page calendar-page'>
+        <DocsHeader title='Calendar 日历' />
+
+        <View className='doc-body'>
+          <View className='panel'>
+            <View className='panel__title'>一般案例</View>
+            <View className='panel__content'>
+              <AtCalendar onMonthChange={this.handleMonthChange} />
+            </View>
+          </View>
+
+          <View className='panel'>
+            <View className='panel__title'>跳转到指定日期</View>
+            <View className='panel__content'>
+              <AtCalendar currentDate={now} />
+              <View className='body_controllers'>
+                <AtButton
+                  size='small'
+                  onClick={this.handleClick.bind(this, 'now', '2018/01/01')}
+                >
+                  跳转到 2018/01/01
+                </AtButton>
+                <AtButton
+                  size='small'
+                  onClick={this.handleClick.bind(this, 'now', '2018/06/18')}
+                >
+                  跳转到 2018/6/18
+                </AtButton>
+              </View>
+            </View>
+          </View>
+
+          <View className='panel'>
+            <View className='panel__title'>指定最小日期和最大日期</View>
+            <View className='panel__content'>
+              <AtCalendar minDate={minDate} maxDate={maxDate} />
+              <View className='body_controllers'>
+                <AtButton
+                  size='small'
+                  onClick={this.handleClick.bind(this, 'minDate', '2018/01/01')}
+                >
+                  设置最小值 2018/1/1
+                </AtButton>
+                <AtButton
+                  size='small'
+                  onClick={this.handleClick.bind(this, 'maxDate', '2019/12/31')}
+                >
+                  设置最大值 2019/12/31
+                </AtButton>
+              </View>
+            </View>
+          </View>
+
+          <View className='panel'>
+            <View className='panel__title'>标记时间</View>
+            <View className='panel__content'>
+              <AtCalendar marks={mark} />
+              <View className='body_controllers'>
+                <AtButton
+                  size='small'
+                  className='button'
+                  onClick={this.handleClick.bind(this, 'mark', [
+                    {
+                      value: Date.now()
+                    }
+                  ])}
+                >
+                  标记当前时间
+                </AtButton>
+              </View>
+            </View>
+          </View>
+
+          <View className='panel'>
+            <View className='panel__title'>禁止滑动</View>
+            <View className='panel__content'>
+              <AtCalendar isSwiper={false} />
+            </View>
+          </View>
+
+          <View className='panel'>
+            <View className='panel__title'>垂直滑动</View>
+            <View className='panel__content'>
+              <AtCalendar isVertical onSelectDate={this.handleDateChange} />
+            </View>
+          </View>
+
+          <View className='panel'>
+            <View className='panel__title'>范围选择</View>
+            <View className='panel__content'>
+              <AtCalendar
+                onSelectDate={this.handleDateChange}
+                isMultiSelect
+                currentDate={multiCurentDate}
+              />
+              <View className='body_controllers'>
+                <AtButton
+                  size='small'
+                  className='button'
+                  onClick={this.handleClick.bind(this, 'multiCurentDate', {
+                    start: '2018/10/28',
+                    end: '2018/11/11'
+                  })}
+                >
+                  设置选择区间为 2018/10/28 - 2018/11/11
+                </AtButton>
+              </View>
+            </View>
+          </View>
+
+          <View className='panel'>
+            <View className='panel__title'>有效时间组</View>
+            <View className='panel__content'>
+              <AtCalendar validDates={validDates} />
+            </View>
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
---
+++ packages/taro-ui-demo/src/pages/form/form/index.tsx
@@ -0,0 +1,116 @@
+import { View } from '@tarojs/components'
+import Taro from '@tarojs/taro'
+import { AtButton, AtCheckbox, AtForm, AtInput, AtToast } from '@taroui/components'
+import { CheckboxOption } from '@taroui/components/types/checkbox'
+
+import DocsHeader from '../../components/doc-header'
+import './index.scss'
+
+interface PageFormState {
+  value1: string
+  value2: string
+  value3: CheckboxOption<string>[]
+  text: string
+  isOpened: boolean
+  [key: string]: string | boolean | CheckboxOption<string>[]
+}
+
+export default class PageForm extends Taro.Component<{}, PageFormState> {
+  public config: Taro.PageConfig = {
+    navigationBarTitleText: 'Taro UI'
+  }
+
+  public constructor () {
+    super(...arguments)
+    this.state = {
+      value1: '',
+      value2: '',
+      value3: [],
+      text: '',
+      isOpened: false
+    }
+  }
+
+  private handleChange (stateName: string, value: any): void {
+    this.setState({
+      [stateName]: value
+    })
+  }
+
+  private handleSubmit (): void {
+    const { value1, value2, value3 } = this.state
+    if (!value1 || !value2 || value3.length < 1) {
+      this.setState({
+        isOpened: true,
+        text: `表单未填写完整`
+      })
+    } else {
+      this.setState({
+        isOpened: true,
+        text: `${value1} / ${value2} / ${value3.join(',')}`
+      })
+    }
+    this.closeToast()
+  }
+
+  private closeToast (): void {
+    setTimeout(() => {
+      this.setState({
+        isOpened: false,
+      })
+    }, 2000)
+  }
+
+  private handleReset (): void {
+    this.setState({
+      isOpened: true,
+      text: `表单已被重置`,
+      value1: '',
+      value2: '',
+      value3: []
+    })
+    this.closeToast()
+  }
+
+  public render (): JSX.Element {
+    return (
+      <View className='page'>
+        <DocsHeader title='Form 表单'></DocsHeader>
+        <View className='doc-body'>
+          {/* 表单提交与重置 */}
+          <View className='panel'>
+            <View className='panel__title'>表单提交与重置</View>
+            <View className='panel__content no-padding'>
+              <View className='component-item'>
+                <AtForm
+                  onSubmit={this.handleSubmit.bind(this)}
+                  onReset={this.handleReset.bind(this)}
+                >
+                  <AtInput name='value1' title='文本' type='text' placeholder='单行文本' value={this.state.value1} onChange={this.handleChange.bind(this, 'value1')} />
+                  <AtInput name='value2' title='密码' type='password' placeholder='请输入密码' value={this.state.value2} onChange={this.handleChange.bind(this, 'value2')} />
+                  <AtCheckbox
+                    options={[
+                      { label: 'iPhone X', value: 'iPhone X' },
+                      { label: 'HUAWEI P20', value: 'HUAWEI P20' }
+                    ]}
+                    selectedList={this.state.value3}
+                    onChange={this.handleChange.bind(this, 'value3')}
+                  />
+                  <View className='component-item__btn-group'>
+                    <View className='component-item__btn-group__btn-item'>
+                      <AtButton type='primary' formType='submit'>提交</AtButton>
+                    </View>
+                    <View className='component-item__btn-group__btn-item'>
+                      <AtButton formType='reset'>重置</AtButton>
+                    </View>
+                  </View>
+                </AtForm>
+              </View>
+            </View>
+          </View>
+        </View>
+        <AtToast text={this.state.text} isOpened={this.state.isOpened}></AtToast>
+      </View>
+    )
+  }
+}
